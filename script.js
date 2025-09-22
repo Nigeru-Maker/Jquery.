3073b04174c3e8a45c22a1d27364ddf3776d5cf1
Nigeru-Maker <qndelacruz03@tip.edu.ph>
2025-09-22 18:10:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
 
     $("#myButton").click(function() {
         alert("Button is Clicked");
-        $("p").slideToggle("slow");
+        $("Text").slideToggle("slow");
     });
 
     $(document).keyup(function(event) {
         if (event.key && event.key.toLowerCase() === 'y') {
-            alert("Letter 'Y' pressed – toggling text");
-            $("p").slideToggle("slow");
+            alert("Letter 'Y' pressed");
+            $("Text").slideToggle("slow");
         }
     });
 
 });
+
